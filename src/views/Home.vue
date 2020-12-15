57d1db5e95e8aee304af79fefe9fae95c7ba5d4e
balbatushka <balbatushka@gmail.com>
2020-12-15 16:27:32 +0300

    todo tutorial

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet libero explicabo ea quidem, blanditiis ipsum animi recusandae veritatis hic nam repellendus fuga quibusdam inventore sed perspiciatis. Distinctio fuga consequuntur voluptates tenetur, tempore labore, officia repudiandae nemo libero, quos saepe aliquam.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
